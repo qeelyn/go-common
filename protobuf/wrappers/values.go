bc6bfcc423a1626fe89ef896fb9717c3ea219d2a
tsingsun <tsingsun@live.cn>
2018-06-08 20:50:51 +0800

    去除wrapper的指针

--- protobuf/wrappers/values.go
+++ protobuf/wrappers/values.go
@@ -1,19 +1,19 @@
 package wrappers
 
-type DoubleValue *float64
+type DoubleValue float64
 
-type FloatValue *float32
+type FloatValue float32
 
-type Int64Value *int64
+type Int64Value int64
 
-type UInt64Value *uint64
+type UInt64Value uint64
 
-type Int32Value *int32
+type Int32Value int32
 
-type UInt32Value *uint32
+type UInt32Value uint32
 
-type BoolValue *bool
+type BoolValue bool
 
-type StringValue *string
+type StringValue string
 
-type BytesValue *[]byte
+type BytesValue []byte
